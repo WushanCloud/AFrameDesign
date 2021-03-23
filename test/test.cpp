2efef04df221218e60b47c695371d321e8fb00aa
WushanCloud <wushanyanyu@126.com>
2021-03-23 17:38:57 +0800

    httplib test

---
+++ test/test.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include "httplib.h"
+
+using namespace std;
+using namespace httplib;
+
+void hello(const Request &req, Response &res) {
+    res.set_content("hello world!", "text/plain");
+}
+
+int main ()
+{
+    Server server;
+    server.Get("/", hello);
+
+    server.listen("0.0.0.0", 9000);
+    while (1);
+    return 0;
+}
