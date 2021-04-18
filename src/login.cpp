c950f62f8cf7b16844df51d9f3d2f55433092ffa
WushanCloud <wushanyanyu@126.com>
2021-04-18 21:14:51 +0800

    token

--- src/login.cpp
+++ src/login.cpp
@@ -22,8 +22,8 @@ void GetFunction(const httplib::Request& req, httplib::Response& res) {
     string err_str = req.method + " " + function_name + " " + req.body;
     Log logerr;
     logerr.GeneralLog(LOG_PATH, err_str);
-
-    CheckToken(req, res);
+    
+    //CheckToken(req, res);
     function_ptr function = GetControll::getInstance()->get_mod(function_name);
     if (function == nullptr) {
         logerr.GeneralLog(ERR_LOG_PATH, err_str);
@@ -46,7 +46,7 @@ void PostFunction(const httplib::Request& req, httplib::Response& res) {
         LoginToken(req, res);
         return ;
     } else {
-        CheckToken(req, res);
+        //CheckToken(req, res);
     }
 
     function_ptr function = PostControll::getInstance()->get_mod(function_name);
@@ -104,13 +104,18 @@ bool LoginToken(const httplib::Request& req, httplib::Response& res) {
     std::string user_number = value["user_number"].asString();
     std::string user_passwd = value["user_passwd"].asString();
     int check_num = check_user(user_type, user_number, user_passwd);
+
+    Json::Value value2; // 用于返回的json
+    Json::FastWriter writer;
     if (check_num == 0) {
         // 为正确的账号密码生成Token，并返回给浏览器
         Token token(user_type, user_number);
         std::cout << token.GetToken() << std::endl;
         res.set_header("Set-Cookie", token.GetToken().c_str());
         res.set_header("Content-Type", "application/json");
-        res.set_redirect("index2.html");
+        // 返回登录的验证结果
+        value2["result"] = "0";
+        res.body = writer.write(value2);
         //登录信息注册到redis
         Redis redis;
         std::string command = "hset " + token.GetToken() + " user_type " + user_type;
@@ -122,25 +127,23 @@ bool LoginToken(const httplib::Request& req, httplib::Response& res) {
         redis.Command("expire " + token.GetToken() + " 3600");
         return true;
     } else {
+        value2["result"] = "1";
+        res.body = writer.write(value2);
         return false;
     }
 }
 
 bool CheckToken(const httplib::Request& req, httplib::Response& res) {
-    Log logerr;
     string err_str;
-    Json::Reader reader;
-    Json::Value value;
-    bool ret = reader.parse(req.body, value);
-    if (ret == false) {
-        logerr.GeneralLog(ERR_LOG_PATH, err_str + "LoginToken Json 解析失败 : " + req.body.c_str());
+    // 获取浏览器页面的token信息
+    std::string token = req.get_header_value("Cookie");
+    // 检测token是否在redis中
+    Redis redis;
+    std::string command = "EXISTS " + token;
+    redisReply* ret = redis.Command(command);
+    if (ret->integer == 0) {
         return false;
     }
-    std::cout << "check_token" << std::endl;
-    std::cout << req.get_header_value_count("Set-Cookie") << std::endl;
-    std::cout << req.get_header_value("Set-Cookie") << std::endl;
-    std::cout << "check_token end" << std::endl;
-
     return true;
 }
 
